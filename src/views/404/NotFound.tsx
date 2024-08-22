49b137f31d02143dcad42504e7f42904d74976c4
Miguel Cobian <miguelcobian99@gmail.com>
2024-08-22 17:10:22 -0600

    Styles. Not found page

--- src/views/404/NotFound.tsx
+++ src/views/404/NotFound.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 export const NotFound = () => {
     return (
         <>
-            <h1 className="font-black text-center text-4xl text-text-light">Page not found</h1>
-            <p className="mt-10 text-center text-text-light">
+            <h1 className="font-black text-center text-4xl text-text-light dark:text-text-dark">Page not found</h1>
+            <p className="mt-10 text-center text-text-light dark:text-text-dark">
                 You might want comeback to {' '}
-                <Link className="text-secondary-dark" to={'/'}>Collections</Link>
+                <Link className="text-secondary-dark dark:text-secondary-light" to={'/'}>Collections</Link>
             </p>
         </>
     )
